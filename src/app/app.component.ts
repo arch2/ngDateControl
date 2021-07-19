b5a2595cea7b62f8a9458ef6bada0e7a3b5742d4
Matt Stratton <mattstrattn@gmail.com>
2021-07-19 15:38:41 -0400

    simple form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  form: FormGroup;
   title = 'app';
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      textInput: this.fb.control("prefill"),
+      dateInputOne: this.fb.control(new Date("06/07/08"), Validators.required),
+      dateInputTwo: this.fb.control({ value: new Date("01/02/03"), disabled: true }),
+      dateInputThree: this.fb.control(new Date(""))
+    })
+  }
 }
